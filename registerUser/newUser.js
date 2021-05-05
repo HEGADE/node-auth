b9424d373a605518531a70eaf79e203448c11cc1
HEGADE <subramanyashegade@gmail.com>
2021-05-05 19:24:05 -0400

    fixed some bugs

--- registerUser/newUser.js
+++ registerUser/newUser.js
@@ -1,8 +1,9 @@
+// middleware helps you to create user and ,one can perform additional logics here like checking for user existence
 let { createToken } = require("../tokenCreation/token");
 module.exports = async (req, res, next) => {
   let { username } = req.body;
   if (!username) {
-    return res.json({ token: null, msg: "username is not provided" });
+    return res.json({ token: null, msg: "Username is not provided/-2" });
   }
   let token = await createToken(username);
 
